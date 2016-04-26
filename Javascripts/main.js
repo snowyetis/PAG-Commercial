86ea9433d47dd74ba48e1e9ed8f78e8767fb52e8
Joe <josephd.carlson@gmail.com>
2016-04-26 19:02:46 -0400

    revised image gallery and minor bug fixes.

--- Javascripts/main.js
+++ Javascripts/main.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
      });
 
     $(".nav a").on("click", function(){
+      console.log('clicked')
       $(".nav").find(".active").removeClass("active");
       $(this).parent().addClass("active");
     });
